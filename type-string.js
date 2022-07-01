da3782e30631b701719cb0a7ffe95cd19da632e2
unknown <pedro.hmachado@hotmail.com>
2022-07-01 10:16:27 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,10 @@
+const texto1 = "Olá mundo";
+const texto2 = 'Olá mundo';
+const senha = "senhaSuperSegura456!"
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é  ";
+const meuNome = "Leonardo";
+
+console.log(citacao + meuNome)
+
